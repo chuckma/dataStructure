4635e6c8a23988211e3715874f0c6cc8b00e5d79
lucas ma <robbincen@163.com>
2018-05-21 21:36:12 +0800

    优先队列

---
+++ src/structure/MaxHeap/PriorityQueue.java
@@ -0,0 +1,41 @@
+package structure.MaxHeap;
+
+/**
+ * @Author lucasma
+ * @Date 2018/5/21 下午8:38
+ * <p>
+ * 优先队列
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+}
---
+++ src/structure/MaxHeap/Queue.java
@@ -0,0 +1,14 @@
+package structure.MaxHeap;
+
+public interface Queue<E> {
+
+    int getSize();
+
+    boolean isEmpty();
+
+    void enqueue(E e);
+
+    E dequeue();
+
+    E getFront();
+}
