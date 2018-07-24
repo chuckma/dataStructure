c897751620dadce5fd4faba6804171d5843b7631
lucas ma <robbincen@163.com>
2018-07-24 22:08:57 +0800

    rbtree

--- src/structure/RBTree/RBTree.java
+++ src/structure/RBTree/RBTree.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
  *
  *
  * 红黑树实现
+ *
+ * 红黑树的特性：
+ *
+ *
  */
 public class RBTree<K extends Comparable<K>, V> {
 
@@ -51,13 +55,66 @@ public class RBTree<K extends Comparable<K>, V> {
         return node.color;
     }
 
+    /**
+     *
+     *     //   node                     x
+     *     //  /   \     左旋转         /  \
+     *     // T1   x   --------->   node   T3
+     *     //     / \              /   \
+     *     //    T2 T3            T1   T2
+     * 左旋转
+     * @param node
+     * @return
+     */
+    private Node leftRotate(Node node) {
+        Node x = node.right;
+        // 左旋转
+        node.right = x.left;
+        x.left = node;
+
+        x.color = node.color;
+
+        node.color = RED;
+        return x;
+    }
+
+    //     node                   x
+    //    /   \     右旋转       /  \
+    //   x    T2   ------->   y   node
+    //  / \                       /  \
+    // y  T1                     T1  T2
+    private Node rightRotate(Node node){
+
+        Node x = node.left;
+
+        // 右旋转
+        node.left = x.right;
+        x.right = node;
+
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+
+    /**
+     * 颜色翻转
+     * @param node
+     */
+    private void flipColors(Node node) {
+
+        node.color = RED;
+        node.left.color=BLACK;
+        node.right.color=BLACK;
+    }
     // 向二分搜索树中添加新的元素(key, value)
     public void add(K key, V value){
         root = add(root, key, value);
+        root.color = BLACK;
     }
 
-    // 向以node为根的二分搜索树中插入元素(key, value)，递归算法
-    // 返回插入新节点后二分搜索树的根
+    // 向以node为根的红黑树中插入元素(key, value)，递归算法
+    // 返回插入新节点后红黑树的根
     private Node add(Node node, K key, V value){
 
         if(node == null){
@@ -71,6 +128,18 @@ public class RBTree<K extends Comparable<K>, V> {
             node.right = add(node.right, key, value);
         else // key.compareTo(node.key) == 0
             node.value = value;
+
+        // 维持红黑树的性质
+        if (isRed(node.right) && !isRed(node.left)) {
+             node = leftRotate(node);
+        }
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rightRotate(node);
+        }
+
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
+        }
 
         return node;
     }
@@ -190,7 +259,8 @@ public class RBTree<K extends Comparable<K>, V> {
         System.out.println("Pride and Prejudice");
 
         ArrayList<String> words = new ArrayList<>();
-        if(FileOperation.readFile("pride-and-prejudice.txt", words)) {
+        String fileName = "/Users/lucasma/IdeaProjects/dataStructure/src/Pride.txt";
+        if(FileOperation.readFile(fileName, words)) {
             System.out.println("Total words: " + words.size());
 
             RBTree<String, Integer> map = new RBTree<>();
