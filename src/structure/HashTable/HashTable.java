632a2515ca3853f1856c8d81298265b6d320df72
lucas ma <robbincen@163.com>
2018-07-28 13:09:23 +0800

    resise hashtable

--- src/structure/HashTable/HashTable.java
+++ src/structure/HashTable/HashTable.java
@@ -7,6 +7,11 @@ import java.util.TreeMap;
  */
 public class HashTable<K, V> {
 
+
+    private static final int upperTol = 10;
+    private static final int lowerTol = 2;
+    private static final int initCapacity = 7;
+
     private TreeMap<K, V> [] hashTable;
     private int M;
     private int size;
@@ -40,6 +45,8 @@ public class HashTable<K, V> {
         } else{
             map.put(key, value);
             size ++;
+            if(size >= upperTol * M)
+                resize(2 * M);
         }
     }
 
@@ -49,6 +56,8 @@ public class HashTable<K, V> {
         if(map.containsKey(key)){
             ret = map.remove(key);
             size --;
+            if(size < lowerTol * M && M / 2 >= initCapacity)
+                resize(M / 2);
         }
         return ret;
     }
@@ -68,5 +77,22 @@ public class HashTable<K, V> {
 
     public V get(K key){
         return hashTable[hash(key)].get(key);
+    }
+
+
+    private void resize(int newM){
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
+        for(int i = 0 ; i < newM ; i ++)
+            newHashTable[i] = new TreeMap<>();
+
+        int oldM = M;
+        this.M = newM;
+        for(int i = 0 ; i < oldM ; i ++){
+            TreeMap<K, V> map = hashTable[i];
+            for(K key: map.keySet())
+                newHashTable[hash(key)].put(key, map.get(key));
+        }
+
+        this.hashTable = newHashTable;
     }
 }
